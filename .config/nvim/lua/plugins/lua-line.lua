f3295d8aec70f5ecef95c22ab30a84218f2ac499
Joshua Tuinder <joshua.tuinder@gmail.com>
2024-04-07 08:28:01 -0600

    initial commit

---
+++ .config/nvim/lua/plugins/lua-line.lua
@@ -0,0 +1,9 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    options = {
+      theme = 'powerline_dark'
+    }
+  end
+}

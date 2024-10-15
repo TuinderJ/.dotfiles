1f6d9bb191a1f62ed993852beabecc479eece34d
TuinderJ <joshua.tuinder@gmail.com>
2024-10-14 20:53:04 -0600

    changed some harpoon keybinds

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -9,6 +9,6 @@ return {
     vim.keymap.set("n", "<C-u>", function() ui.nav_file(1) end)
     vim.keymap.set("n", "<C-i>", function() ui.nav_file(2) end)
     vim.keymap.set("n", "<C-o>", function() ui.nav_file(3) end)
-    vim.keymap.set("n", "<C-r>", function() ui.nav_file(4) end)
+    vim.keymap.set("n", "<C-y>", function() ui.nav_file(4) end)
   end
 }
--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -34,6 +34,8 @@ return {
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
+
     end,
   },
   {
